0094e4adc9932b506a4862a868ad19453910f365
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-23 22:57:45 +0600

    poduct

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,120 +3,30 @@ import { FaArrowRight } from 'react-icons/fa';
 
 const Products = () => {
    return (
-      <div className='bg-slate-100 py-14'>
-         <div>
-            <h6 className='font-medium text-center'>What we offer for you</h6>
-            <h1 className='text-6xl font-medium text-center mb-14'> <span className='text-orange-500'>New</span> Products</h1>
-         </div>
-         <div className='flex items-center justify-center container mx-auto'>
-            <div className='grid gap-6 grid-cole-1 lg:grid-cols-2'>
-
-               <div className='rounded-xl shadow-xl bg-white'>
-                  <div className='p-5 flex flex-col'>
-                     <div className='rounded-xl overflow-hidden flex'>
-                        <div className='w-[25%]'>
-                           <img className='m-auto' src="https://rb.gy/py18s" alt="" />
-                        </div>
-                        <div className='w-[75%] ml-8'>
-                           <h1 className='text-2xl font-medium mb-6 text-green-500'>Pet Sitting</h1>
-                           <p className='font-medium'>Event time you check your cat or pet, we will definitely check and groom your animal at the some time</p>
-                           <div className='flex'>
-                              <p className='mt-5 font-bold'>Read More</p>
-                              <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+      <div className='container m-auto'>
+         <div className='mt=14 mb-14'>
+            <h1 className='text-4xl text-center'>Frequently Asked Questions</h1>
+            <p className='text-center mt-2'>Find your answer</p>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-4 text-center'>
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+                  <div></div>
+                  <img className='h-20 m-auto' src="https://i.ibb.co/zGkdbBT/best.png" alt="" />
+                  <h3 className='my-3 text-xl font-bold'>Best Service</h3>
                </div>
-
-               <div className='rounded-xl shadow-xl bg-white'>
-                  <div className='p-5 flex flex-col'>
-                     <div className='rounded-xl overflow-hidden flex'>
-                        <div className='w-[25%]'>
-                           <img className='m-auto' src="https://rb.gy/utjsm" alt="" />
-                        </div>
-                        <div className='w-[75%] ml-8'>
-                           <h1 className='text-2xl font-medium mb-6 text-green-500'>Pet Grooming</h1>
-                           <p className='font-medium'>Event time you check your cat or pet, we will definitely check and groom your animal at the some time</p>
-                           <div className='flex'>
-                              <p className='mt-5 font-bold'>Read More</p>
-                              <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg' >
+                  {/* <div></div> */}
+                  <img className='h-20 m-auto' src="https://i.ibb.co/QMcGs4c/value.png" alt="" />
+                  <h3 className='my-3 text-xl font-bold'>Best Money Product</h3>
                </div>
-
-               <div className='rounded-xl shadow-xl bg-white'>
-                  <div className='p-5 flex flex-col'>
-                     <div className='rounded-xl overflow-hidden flex'>
-                        <div className='w-[25%]'>
-                           <img className='m-auto' src="https://rb.gy/k5ho8" alt="" />
-                        </div>
-                        <div className='w-[75%] ml-8'>
-                           <h1 className='text-2xl font-medium mb-6 text-green-500'>Pet Adoption</h1>
-                           <p className='font-medium'>Event time you check your cat or pet, we will definitely check and groom your animal at the some time</p>
-                           <div className='flex'>
-                              <p className='mt-5 font-bold'>Read More</p>
-                              <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg'>
+                  <div></div>
+                  <img className='h-20 m-auto' src="https://i.ibb.co/P1gjQSy/material.png" alt="" />
+                  <h3 className='my-3 text-xl font-bold'>High Quality</h3>
                </div>
-
-               <div className='rounded-xl shadow-xl bg-white'>
-                  <div className='p-5 flex flex-col'>
-                     <div className='rounded-xl overflow-hidden flex'>
-                        <div className='w-[25%]'>
-                           <img className='m-auto' src="https://rb.gy/ig0pg" alt="" />
-                        </div>
-                        <div className='w-[75%] ml-8'>
-                           <h1 className='text-2xl font-medium mb-6 text-green-500'>Natural Products</h1>
-                           <p className='font-medium'>Event time you check your cat or pet, we will definitely check and groom your animal at the some time</p>
-                           <div className='flex'>
-                              <p className='mt-5 font-bold'>Read More</p>
-                              <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-
-               <div className='rounded-xl shadow-xl bg-white'>
-                  <div className='p-5 flex flex-col'>
-                     <div className='rounded-xl overflow-hidden flex'>
-                        <div className='w-[25%]'>
-                           <img className='m-auto' src="https://rb.gy/wpsde" alt="" />
-                        </div>
-                        <div className='w-[75%] ml-8'>
-                           <h1 className='text-2xl font-medium mb-6 text-green-500'>Pet Vaccination</h1>
-                           <p className='font-medium'>Event time you check your cat or pet, we will definitely check and groom your animal at the some time</p>
-                           <div className='flex'>
-                              <p className='mt-5 font-bold'>Read More</p>
-                              <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-
-               <div className='rounded-xl shadow-xl bg-white'>
-                  <div className='p-5 flex flex-col'>
-                     <div className='rounded-xl overflow-hidden flex'>
-                        <div className='w-[25%]'>
-                           <img className='m-auto' src="https://rb.gy/11ec6" alt="" />
-                        </div>
-                        <div className='w-[75%] ml-8'>
-                           <h1 className='text-2xl font-medium mb-6 text-green-500'>Pet Foods</h1>
-                           <p className='font-medium'>Event time you check your cat or pet, we will definitely check and groom your animal at the some time</p>
-                           <div className='flex'>
-                              <p className='mt-5 font-bold'>Read More</p>
-                              <p className='mt-6 ml-3'><FaArrowRight></FaArrowRight></p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg'>
+                  <div></div>
+                  <img className='h-20 m-auto' src="https://i.ibb.co/JRBzFvQ/support.png" alt="" />
+                  <h3 className='my-3 text-xl font-bold'>24/7 Best Support</h3>
                </div>
             </div>
          </div>
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -7,28 +7,29 @@ const Services = () => {
 
       <div className=' mb-14'>
          <h1 className='text-center mt-14 text-4xl font-bold'>Our service</h1>
+         <p className='text-center mt-4 font-bold mb-5'>All new upcoming Robort in the world</p>
          <div className='flex  justify-center gap-5'>
             <div className="card w-96 bg-base-100 shadow-xl">
                <figure><img src="https://rb.gy/j7iw9" alt="Shoes" /></figure>
                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <h2 className="card-title">RoboBuilder Kit</h2>
+                  <p>The RoboBuilder Kit is an all-in-one robotics construction set that empowers aspiring roboticists and enthusiasts to bring their imagination to life. </p>
                </div>
 
             </div>
             <div className="card w-96 bg-base-100 shadow-xl">
                <figure><img src="https://rb.gy/j7iw9" alt="Shoes" /></figure>
                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <h2 className="card-title">RoboGenius Kit</h2>
+                  <p>The RoboGenius Kit is an exciting and comprehensive robotics kit designed to unleash your creativity and intellect.</p>
                </div>
 
             </div>
             <div className="card w-96 bg-base-100 shadow-xl">
                <figure><img src="https://rb.gy/j7iw9" alt="Shoes" /></figure>
                <div className="card-body">
-                  <h2 className="card-title">Shoes!</h2>
-                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <h2 className="card-title">TechnoBotics Kit</h2>
+                  <p>The TechnoBotics Kit is a dynamic and innovative robotics kit that merges technology and robotics to provide an immersive learning experience. </p>
                </div>
 
             </div>
