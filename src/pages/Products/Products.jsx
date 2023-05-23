e9351ebca4ecc1879910ef0706bc8c4f204e11d4
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-23 23:06:19 +0600

    serverc aos

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -6,25 +6,25 @@ const Products = () => {
       <div className='container m-auto'>
          <div className='mt=14 mb-14'>
             <h1 className='text-4xl text-center'>Frequently Asked Questions</h1>
-            <p className='text-center mt-2'>Find your answer</p>
+            <p className='text-center mt-2 mb-9'>Find your answer</p>
             <div className='grid grid-cols-2 md:grid-cols-4 gap-4 text-center'>
                <div className='bg-sky-100 p-5 rounded-lg shadow-lg' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
-                  <div></div>
+
                   <img className='h-20 m-auto' src="https://i.ibb.co/zGkdbBT/best.png" alt="" />
                   <h3 className='my-3 text-xl font-bold'>Best Service</h3>
                </div>
-               <div className='bg-sky-100 p-5 rounded-lg shadow-lg' >
-                  {/* <div></div> */}
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+
                   <img className='h-20 m-auto' src="https://i.ibb.co/QMcGs4c/value.png" alt="" />
                   <h3 className='my-3 text-xl font-bold'>Best Money Product</h3>
                </div>
-               <div className='bg-sky-100 p-5 rounded-lg shadow-lg'>
-                  <div></div>
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+
                   <img className='h-20 m-auto' src="https://i.ibb.co/P1gjQSy/material.png" alt="" />
                   <h3 className='my-3 text-xl font-bold'>High Quality</h3>
                </div>
-               <div className='bg-sky-100 p-5 rounded-lg shadow-lg'>
-                  <div></div>
+               <div className='bg-sky-100 p-5 rounded-lg shadow-lg' data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+
                   <img className='h-20 m-auto' src="https://i.ibb.co/JRBzFvQ/support.png" alt="" />
                   <h3 className='my-3 text-xl font-bold'>24/7 Best Support</h3>
                </div>
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -9,7 +9,7 @@ const Services = () => {
          <h1 className='text-center mt-14 text-4xl font-bold'>Our service</h1>
          <p className='text-center mt-4 font-bold mb-5'>All new upcoming Robort in the world</p>
          <div className='flex  justify-center gap-5'>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 bg-base-100 shadow-xl " data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
                <figure><img src="https://rb.gy/j7iw9" alt="Shoes" /></figure>
                <div className="card-body">
                   <h2 className="card-title">RoboBuilder Kit</h2>
@@ -17,7 +17,7 @@ const Services = () => {
                </div>
 
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 bg-base-100 shadow-xl" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
                <figure><img src="https://rb.gy/j7iw9" alt="Shoes" /></figure>
                <div className="card-body">
                   <h2 className="card-title">RoboGenius Kit</h2>
@@ -25,7 +25,7 @@ const Services = () => {
                </div>
 
             </div>
-            <div className="card w-96 bg-base-100 shadow-xl">
+            <div className="card w-96 bg-base-100 shadow-xl" data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
                <figure><img src="https://rb.gy/j7iw9" alt="Shoes" /></figure>
                <div className="card-body">
                   <h2 className="card-title">TechnoBotics Kit</h2>
