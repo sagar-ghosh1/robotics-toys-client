7b2ce3582b09fcac81a89e980e8fe02f6f817f3d
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-23 17:09:27 +0600

    gallery done

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Gallery = () => {
     return (
         <div className='mt-24 mb-24 text-center'>
-            <h5 className='text-2xl font-bold'>Photo Gallery</h5>
-            <h2 className='text-4xl font-bold'>kids with Robort toy</h2>
-            <div className='grid lg:grid-cols-4 grid-cols-1 gap-7 max-w-[1200px] mx-auto' >
+            <h5 className='text-2xl font-bold text-purple-500 mb-2'>Photo Gallery</h5>
+            <h2 className='text-4xl font-bold mb-8'>kids with Robort toy</h2>
+            <div className='grid lg:grid-cols-4 grid-cols-1 gap-7 max-w-[1200px] mx-auto' data-aos="fade-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
                 <div>
                     <img className='rounded-md' src="https://rb.gy/2qsfy" alt="" />
                 </div>
@@ -13,7 +13,7 @@ const Gallery = () => {
                     <img className='rounded-md' src="https://rb.gy/t60xr" alt="" />
                 </div>
                 <div className='imgBox'>
-                    <img className='rounded-md' src="https://rb.gy/xtlu2" alt="" />
+                    <img className='rounded-md' src="https://rb.gy/s2zi9" alt="" />
                 </div>
                 <div className='imgBox'>
                     <img className='rounded-md' src="https://rb.gy/z4mq8" alt="" />
@@ -29,6 +29,18 @@ const Gallery = () => {
                 </div>
                 <div className='imgBox'>
                     <img className='rounded-md' src="https://rb.gy/t9hb6" alt="" />
+                </div>
+                <div className='imgBox'>
+                    <img className='rounded-md' src="https://rb.gy/q9a87" alt="" />
+                </div>
+                <div className='imgBox'>
+                    <img className='rounded-md' src="https://rb.gy/676uw" alt="" />
+                </div>
+                <div className='imgBox'>
+                    <img className='rounded-md' src="https://rb.gy/lbh9z" alt="" />
+                </div>
+                <div className='imgBox'>
+                    <img className='rounded-md' src="https://rb.gy/rnij4" alt="" />
                 </div>
             </div>
         </div>
